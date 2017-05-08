ea154cfc7fca83a58da863de63b8763c4723e8ad
ThomasLiao <liao123abc@yeah.net>
2017-05-08 22:29:56 +0800

    add bubble sort

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -62,8 +62,20 @@ Java_liao_thomas_javadev_ndk_NdkTest_stringFromJNI(JNIEnv* env, jobject thiz) {
     for (int i = 0; i < 7; i++) {
         k[i] = '5';
     }
+
     //return env->NewStringUTF("Hello from JNI !  Compiled with ABI " ABI ".");
     return test222(env);
+}
+
+char* convertArrayInt2Char(int a[], int n)
+{
+    //输出为char类型
+    char* b = new char[n];
+    for (int j = 0; j < n; j++)
+    {
+        b[j] = a[j] + '0';
+    }
+    return b;
 }
 
 /**
@@ -81,6 +93,10 @@ Java_liao_thomas_javadev_ndk_NdkTest_stringFromJNI(JNIEnv* env, jobject thiz) {
  * 4 重复步骤3，直到找到已排序的元素小于或者等于新元素的位置
  * 5 将新元素插入到该位置后
  * 6 重复步骤2~5
+ *
+ * 最坏情况下的比较次数是 1 + 2 + 3 + ... + (N - 1)，等差数列求和，结果为 N^2 / 2，所以最坏情况下的复杂度为 O(N^2)。
+ * 最好情况下，数组已经是有序的，每插入一个元素，只需要考查前一个元素，因此最好情况下，插入排序的时间复杂度为O(N)
+ *
  *
  *     分类 ------------- 内部比较排序
  *     数据结构 ---------- 数组
@@ -109,19 +125,51 @@ char* InsertSort(int a[], int n)
         a[j + 1] = get;//赋值到适当位置
     }
 
-    //输出为char类型
-    char* b = new char[n];
-    for (int j = 0; j < n; j++)
+    char * b = convertArrayInt2Char(a, n);
+    return b;
+}
+
+
+void exchange(int a[], int i, int j)
+{
+    int temp = a[i];
+    a[i] = a[j];
+    a[j] = temp;
+}
+
+/*比较相邻的元素，如果前一个比后一个大，就把它们两个调换位置。
+对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。这步做完后，最后的元素会是最大的数。
+针对所有的元素重复以上的步骤，除了最后一个。
+持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
+ */
+// 分类 -------------- 内部比较排序
+// 数据结构 ---------- 数组
+// 最差时间复杂度 ---- O(n^2)
+// 最优时间复杂度 ---- 如果能在内部循环第一次运行时,使用一个旗标来表示有无需要交换的可能,可以把最优时间复杂度降低到O(n)
+// 平均时间复杂度 ---- O(n^2)
+// 所需辅助空间 ------ O(1)
+// 稳定性 ------------ 稳定
+char* BubbleSort(int a[], int n) {
+    for (int j = 0; j < n - 1; j++)// 每次最大元素就像气泡一样"浮"到数组的最后
     {
-        b[j] = a[j] + '0';
+        for (int i = 0; i < n - 1 - j; i++)// 依次比较相邻的两个元素,使较大的那个向后移
+        {
+            if (a[i] > a[i + 1])
+            {
+                exchange(a, i, i + 1);
+            }
+        }
     }
+
+    char * b = convertArrayInt2Char(a, n);
     return b;
 }
 
 jstring test222(JNIEnv* env){
     int a[8] = {3,1,5,7,2,4,9,6};
-    char* d = InsertSort(a,8);
 
+    //char* d = InsertSort(a,8);
+    char* d = BubbleSort(a,8);
     jstring result = stoJstring(env, d);
     return result;
 }
